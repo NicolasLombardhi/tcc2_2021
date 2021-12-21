ad67ba252544929476052cf81988a07d05025941
NicolasLombardhi <89940672+NicolasLombardhi@users.noreply.github.com>
2021-12-20 22:24:18 -0300

    Add files via upload

---
+++ JS/scripts.js
@@ -0,0 +1,14 @@
+function fodase(){
+
+    alert("Seja bem vindo!")
+
+}    
+function SENHA(){
+    alert(" Nova senha enviada ao seu e-mail.")
+}
+function go(){
+    alert(" Usuário cadastrado com sucesso! Faça o login")
+}
+function politica(){
+    alert("Concordo com a política de privacidade:")
+}
